dd252be4e6f8e40e533c3710ab6f25f1d21b3002
Bailey Sisomphane <bsisomphane7@gmail.com>
2022-02-01 12:37:02 -0600

    changes to folder structure/ added boiler plates

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route component={NoMatch} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
