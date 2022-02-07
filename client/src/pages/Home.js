f91d5198130ba83605e9ddb24f07280cd02cb17d
Alex.Ramirez <aleramirez201@gmail.com>
2022-02-07 07:00:00 -0600

    added calander and changed naming

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,17 +4,31 @@ import { useQuery } from '@apollo/client';
 import ThoughtList from '../components/ThoughtList';
 import ThoughtForm from '../components/ThoughtForm';
 import TodoForm from '../components/TodoForm';
-import TodoList from '../components/TodoList';
+// import TodoList from '../components/TodoList';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 import { QUERY_THOUGHTS } from '../utils/queries';
 
-const Home = () => {
+function Home() { 
+  const [date, setDate] = useState(new Date());
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const thoughts = data?.thoughts || [];
 
   return (
     <main>
       <div className="flex-row justify-center">
+      <div className='app'>
+      <h1 className='text-center'>Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+    </div>
         <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
@@ -34,7 +48,7 @@ const Home = () => {
           ) : (
             <ThoughtList
               thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
+              title="Here are your ToDo's and Notes"
             />
           )}
         </div>
@@ -51,5 +65,6 @@ const Home = () => {
     </main>
   );
 };
+
 
 export default Home;
